2ba754d604b64d6863dfeada28cf1a3f9b41b79d
Lucio Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2024-03-14 15:17:14 -0400

    Creacion del Modelo de Authors

---
+++ app/Models/Author.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name', 'nationality', 'created_at', 'updated_at', 'created_by', 'updated_by'
+    ];
+
+}
