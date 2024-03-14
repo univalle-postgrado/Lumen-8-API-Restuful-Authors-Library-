ff17cfa8ee19da4dcf1578dc0924315a861a01dc
Lucio Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2024-03-14 01:59:26 -0400

    Creacion del migration para authors

---
+++ database/migrations/2024_03_14_055638_create_authors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60);
+            $table->string('nationality', 30)->nullable();
+            $table->timestamps();
+            $table->string('created_by');
+            $table->string('updated_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
